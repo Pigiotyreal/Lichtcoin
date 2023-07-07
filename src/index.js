010122d0571693b0758619e769fde1368920ac03
Pigioty <Pigiotyreal@gmail.com>
2023-07-07 12:23:56 -0400

    transaction class

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ class Block {
         }
 
         return difficulty
-    }
+    } 
 }
 
 class Blockchain {
@@ -95,6 +95,75 @@ class Blockchain {
         }
 
         return true
+    }
+}
+
+class Transaction {
+    constructor(from, to, amount) {
+        this.from = from
+        this.to = to
+        this.amount = amount
+    }
+
+    static isValid(transaction) {
+        if (typeof transaction.from != "string") {
+            return false
+        } else if (typeof transaction.to != "string") {
+            return false
+        } else if (typeof transaction.amount != "number") {
+            return false
+        }
+
+        return true
+    }
+
+    static sign(transaction, privateKey) {
+        transaction.signature = crypto.createSign("SHA256").update(transaction.from + transaction.to + transaction.amount).sign(privateKey, "hex")
+    }
+
+    static verify(transaction) {
+        const publicKey = crypto.createPublicKey(transaction.from)
+        return crypto.createVerify("SHA256").update(transaction.from + transaction.to + transaction.amount).verify(publicKey, transaction.signature)
+    }
+
+    static transfer(from, to, amount, privateKey) {
+        const transaction = new Transaction(from, to, amount)
+        Transaction.sign(transaction, privateKey)
+        return transaction
+    }
+
+    static generateKeyPair() {
+        return crypto.generateKeyPairSync("rsa", {
+            modulusLength: 2048,
+            publicKeyEncoding: {
+                type: "spki",
+                format: "pem"
+            },
+            privateKeyEncoding: {
+                type: "pkcs8",
+                format: "pem"
+            }
+        })
+    }
+
+    static get publicKey() {
+        return this.generateKeyPair().publicKey
+    }
+
+    static get privateKey() {
+        return this.generateKeyPair().privateKey
+    }
+
+    static get wallet() {
+        const keyPair = this.generateKeyPair()
+        return {
+            publicKey: keyPair.publicKey,
+            privateKey: keyPair.privateKey
+        }
+    }
+
+    static get balance() {
+        return this.amount
     }
 }
 
