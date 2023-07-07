eada14a495d9a859efd46ec4ae42960d1e961751
Pigioty <Pigiotyreal@gmail.com>
2023-07-07 10:45:03 -0400

    blockchain class

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class Block {
         const hash = crypto.createHash("sha256").update("Genesis Block").digest("hex")
         const data = "Genesis Block"
         const timestamp = 0
-        const difficulty = 4
+        const difficulty = 5
         const nonce = 0
 
         return new Block(index, prevHash, hash, data, timestamp, difficulty, nonce)
@@ -55,9 +55,41 @@ class Block {
     }
 }
 
+class Blockchain {
+    constructor() {
+        this.blocks = [Block.genesis]
+    }
+
+    addBlock(data) {
+        const prevBlock = this.blocks[this.blocks.length - 1]
+        const block = Block.mine(prevBlock, data)
+        this.blocks.push(block)
+    }
+
+    isValid() {
+        for (let i = 1; i < this.blocks.length; i++) {
+            const block = this.blocks[i]
+            const prevBlock = this.blocks[i - 1]
+
+            if (Block.isValid(block, prevBlock) == false) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
+
 const genesisBlock = Block.genesis
 console.log(genesisBlock)
 
 const block = Block.mine(genesisBlock, "Hello World")
-console.log(Block.isValid(block, genesisBlock))
 console.log(block)
+
+const blockchain = new Blockchain()
+blockchain.addBlock("Hello World")
+blockchain.addBlock("Hello World")
+blockchain.addBlock("Hello World")
+console.log(blockchain)
+
+console.log(blockchain.isValid())
