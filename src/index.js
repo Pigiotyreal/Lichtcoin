41a07c652cced7ae27a1cf7225e0cd6533dd7894
Pigioty <Pigiotyreal@gmail.com>
2023-07-07 10:30:12 -0400

    genesis block

--- src/index.js
+++ src/index.js
@@ -10,4 +10,15 @@ class Block {
         this.difficulty = difficulty
         this.nonce = nonce
     }
+
+    static get genesis() {
+        const index = 0
+        const prevHash = "0".repeat(64)
+        const data = "Genesis Block"
+        const timestamp = 0
+        const difficulty = 0
+        const nonce = 0
+
+        return new Block(index, prevHash, data, timestamp, difficulty, nonce)
+    }
 }
