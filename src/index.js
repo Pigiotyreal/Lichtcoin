128383987e2f43bd4ae4f92aa85c6193a0babb47
Pigioty <Pigiotyreal@gmail.com>
2023-07-07 10:29:37 -0400

    workin on block class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hi.")
+const crypto = require("crypto")
+
+class Block {
+    constructor(index, prevHash, hash, data, timestamp, difficulty, nonce) {
+        this.index = index
+        this.prevHash = prevHash
+        this.hash = hash
+        this.data = data
+        this.timestamp = timestamp
+        this.difficulty = difficulty
+        this.nonce = nonce
+    }
+}
